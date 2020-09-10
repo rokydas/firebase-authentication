4b3889138b7ac19d38d867918d7e94442003e0b1
rokydas <droky136@gmail.com>
2020-09-10 22:53:25 +0600

    email and password validation added

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ function App() {
     isSignedIn: false, 
     name: '', 
     email: '', 
+    password: '',
     photo: ''
   })
 
@@ -36,6 +37,7 @@ function App() {
         isSignedIn: true,
         name: displayName,
         email: email,
+        password: '',
         photo: photoURL
       }
       
@@ -54,6 +56,7 @@ function App() {
         isSignedIn: false,
         name: '',
         email: '',
+        password: '',
         photo: ''
       }
       setUser(user);
@@ -62,6 +65,62 @@ function App() {
     .catch(error => {
       console.log(error);
     })
+  }
+
+  let isEmailValid, isPasswordValid;
+
+
+  // My trying
+  // const handleBlur = (event) => {
+    
+  //   // console.log(event.target.name, event.target.value);
+  //   if(event.target.name === 'email'){
+  //     isEmailValid = /\S+@\S+\.\S+/.test(event.target.value);
+  //   }
+  //   if(event.target.name === 'password'){
+  //     isPasswordValid = event.target.value.length >= 6 && /\d{1}/.test(event.target.value);
+  //   }
+  //   if(isEmailValid && event.target.name === 'email'){
+  //     const newUser = {...user};
+  //     newUser[event.target.name] = event.target.value;
+  //     setUser(newUser);
+  //   }
+  //   if(isPasswordValid  && event.target.name === 'password'){
+  //     const newUser = {...user};
+  //     newUser[event.target.name] = event.target.value;
+  //     setUser(newUser);
+  //   }
+  //   else{
+  //     console.log('jhamala ase');
+  //   }
+  // }
+
+  // According to jhankar vhai
+
+  const is_valid_email = email =>  /(.+)@(.+){2,}\.(.+){2,}/.test(email); 
+  const hasNumber = input => /\d/.test(input);
+
+  const handleBlur = (event) =>{
+    const newUserInfo = {
+      ...user
+    };
+    //debugger;
+    // perform validation
+    let isValid = true;
+    if(event.target.name === 'email'){
+      isValid = is_valid_email(event.target.value);
+    }
+    if(event.target.name === "password"){
+      isValid = event.target.value.length > 8 && hasNumber(event.target.value);
+    }
+
+    newUserInfo[event.target.name] = event.target.value;
+    newUserInfo.isValid = isValid;
+    setUser(newUserInfo);
+  }
+
+  const handleSubmit = () => {
+
   }
 
   return (
@@ -77,8 +136,25 @@ function App() {
           <h3>Your Email Address: {user.email}</h3>
           <img src={user.photo} alt=""/>
         </div>
-        
       }
+
+      {/* <h1>Our own authentication</h1>
+      <h3>Your Email Address: {user.email}</h3>
+      <h3>Your password: {user.password}</h3> */}
+      
+      {/* onChange
+      <form onSubmit={handleSubmit}>
+        <input name="email" type="text" onChange={handleChange} placeholder="Your Email address" required/><br/>
+        <input name="password" type="password" onChange={handleChange} placeholder="Password" id="" required/><br/>
+        <input type="submit" value="Sign In"/>
+      </form> */}
+
+      {/* onBlur */}
+      <form onSubmit={handleSubmit}>
+        <input name="email" type="text" onBlur={handleBlur} placeholder="Your Email address" required/><br/>
+        <input name="password" type="password" onBlur={handleBlur} placeholder="Password" id="" required/><br/>
+        <input type="submit" value="Sign In"/>
+      </form>
     </div>
   );
 }
