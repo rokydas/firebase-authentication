d5c389bc9aaa7d232cc6f24cff3e157535966a73
rokydas <droky136@gmail.com>
2020-09-11 18:54:55 +0600

    facebook log in added

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ firebase.initializeApp(firebaseConfig);
 function App() {
   
   const provider = new firebase.auth.GoogleAuthProvider();
+  const fbProvider = new firebase.auth.FacebookAuthProvider();
 
   const btnStyle = {
-    color: 'white',
-    backgroundColor: 'black',
+    notiColor: 'white',
+    backgroundnotiColor: 'black',
     padding: '10px 15px',
     fontSize: '20px',
     borderRadius: '10px',
@@ -28,7 +29,8 @@ function App() {
   })
 
   const [noti, setNoti] = useState('');
-  const [color, setColor] = useState('');
+  const [notiColor, setNotiColor] = useState('');
+  const [isNew, setIsNew] = useState(false);
 
   const handleSignIn = () => {
     console.log("it's working YAY");
@@ -89,32 +91,87 @@ function App() {
   }
 
   const handleSubmit = (event) => {
-    if(user.email && user.password){
+    if(isNew && user.email && user.password){
       firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
       .then(res => {
         console.log(res);
         setNoti('User created successfully');
-        setColor('green');
+        setNotiColor('green');
+        updateUserProfile(user.name);
       })
       .catch(error => {
         // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
+        const errorCode = error.code;
+        const errorMessage = error.message;
         console.log(errorCode, errorMessage);
         // ...
         setNoti(errorMessage)
-        setColor('red');
+        setNotiColor('red');
+      });
+    }
+    if(!isNew && user.email && user.password){
+      firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+      .then(res => {
+        console.log(res.user.displayName);
+        setNoti('Sign in successful');
+        setNotiColor('green');
+      })
+      .catch(function(error) {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        setNoti(errorMessage);
+        setNotiColor('red')
+        // ...
       });
     }
     event.preventDefault();
+  }
+
+  const handleIsNew = () => {
+    setIsNew(!isNew);
+  }
+
+  const updateUserProfile = (name) => {
+    const user = firebase.auth().currentUser;
+
+    user.updateProfile({
+      displayName: name
+    }).then(function() {
+      console.log('update successful');
+    }).catch(function(error) {
+      console.log(error);
+    });
+  }
+
+  const handleFbSignIn = () => {
+    firebase.auth().signInWithPopup(fbProvider)
+    .then(function(result) {
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+    })
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
   }
 
   return (
     <div className="app">
       {
         user.isSignedIn ? <button onClick={handleSignOut} style={btnStyle}>Sign out</button> 
-        : <button onClick={handleSignIn} style={btnStyle}>Sign in</button>
+        : <button onClick={handleSignIn} style={btnStyle}>Sign in with Google</button>
       }
+      <br/><button onClick={handleFbSignIn} style={btnStyle} >Sign in with Facebook</button>
       
       {
         user.isSignedIn && <div>
@@ -136,13 +193,17 @@ function App() {
       </form> */}
 
       {/* onBlur */}
+      <br/>
+      <input type="checkbox" onChange={handleIsNew} name="" id=""/>
+      <label htmlFor="newUser">New User sign up</label>
       <form onSubmit={handleSubmit}>
+      {isNew && <input type="text" name="name" onBlur={handleBlur} id="" placeholder="Your name"/>}<br/>
         <input name="email" type="text" onBlur={handleBlur} placeholder="Your Email address" required/><br/>
         <input name="password" type="password" onBlur={handleBlur} placeholder="Password" id="" required/><br/>
         <input type="submit" onClick={handleSubmit} value="Sign In"/>
       </form>
       {
-        <h3 style={{color: color}}>{noti}</h3>
+        <h3 style={{color: notiColor}}>{noti}</h3>
       }
     </div>
   );
